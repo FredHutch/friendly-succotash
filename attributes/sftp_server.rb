7c39bd1ecbd2289bfd4956cffba964e100bc725e
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-03-09 17:23:51 -0800

    Add sftp-only configuration for sshd
     - seems to work in limited testing.  server refuses ssh connections
     - still need to manage data_dir.  Needs to be created owned by root
       with permissions to subdirectories allowing upload.

--- attributes/sftp_server.rb
+++ attributes/sftp_server.rb
@@ -2,5 +2,6 @@ node.default['sftp_server'] = {
   'device' => "#{node['network']['default_interface']}:1",
   'inet_addr' => '172.16.3.3',
   'network' => '172.16.3.1',
-  'netmask' => '255.255.255.0'
+  'netmask' => '255.255.255.0',
+  'data_dir' => '/var/opt/sftp'
 }
